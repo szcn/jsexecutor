0cb5f846690802555d375367e677aa1a7a63a226
Sezai Can <sezai.can@sahibinden.com>
2020-04-30 18:37:12 +0300

    relase sh fixed

--- merge_release_to_master.sh
+++ merge_release_to_master.sh
@@ -14,14 +14,16 @@ cd jsexecutor-core/ || exit 1
 
 echo "$PWD"
 
-mvn --settings ~/.m2/${settings}.xml clean install -Dgpg.skip
+mvn --settings ~/.m2/"${settings}".xml clean install -Dgpg.skip
 
 echo "Singing and deploy to maven"
+
+path="$PWD"/target/jsexecutor-core-"${version}"
 
 i=1
 for jar in jar, pom, javadoc.jar, sources.jar; do
 
-  gpg --passphrase "${passphrase}" --output target/jsexecutor-core-"${version}".$jar.sig --sign target/jsexecutor-core-"${version}".$jar
+ gpg --passphrase "${passphrase}" --output "${path}".$jar.sig --sign "${path}".$jar
 
   echo "Sign : $jar"
 
