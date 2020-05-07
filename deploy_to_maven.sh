04148a50104423971db27fa8c8f4610a1690f396
Sezai Can <sezai.can@sahibinden.com>
2020-05-07 18:41:59 +0300

    deploy to maven

--- deploy_to_maven.sh
+++ deploy_to_maven.sh
@@ -4,9 +4,11 @@ version=$1
 settings=$2
 passphrase=$3
 
-sh create_maven_release.sh version settings passphrase
+sh create_maven_release.sh "$version" "$settings" "$passphrase"
 
 echo "Deploy to mavan."
+
+cd jsexecutor-core/ || exit 1
 
 mvn --settings ~/.m2/"${settings}".xml clean deploy
 
